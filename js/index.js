1c9724a4ca1fbc864cd75b1390e93850c0e4d474
Manuel Vargas <manuelvargas@my.unt.edu>
2018-02-25 14:07:08 -0600

    clean up 🛠

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// cannot put blotter js code in here because scope.appendTo(elem); gives error
+// cannot put blotter js code in here because scope.appendTo(elem) gives error
 
 
 // mapping function to translate the pages X coordinate to the text graphic range
@@ -6,20 +6,27 @@ Number.prototype.map = function (in_min, in_max, out_min, out_max) {
     return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-
+// mouse event handler
 // on html, change material offset according to mouse x postion
 $("html").mousemove(function (event) {
-    //console.log("pageX: " + event.pageX)
 
+    // print xpos
+    // console.log("pageX: " + event.pageX)
+
+    // round the mouse position
     let xpos = Math.ceil(event.pageX / 10) * 10
 
+    // map the ranges of the window width with the offset value
     let mouseValue = xpos.map(
+        // window x0, xf
         0, $(window).width(),
-        0.0186, 0.1)
 
-    //console.log("pageX mod 1: " + mouseValue)
+        // blotter material offset x0, xf
+        0.0186, 0.1
+    )
 
-    // material.uniforms.uOffset.value = event.pageX % 1
+    //console.log("pageX': " + mouseValue)
+
+    // set the material offset to the mouse value
     material.uniforms.uOffset.value = mouseValue
-
 })
